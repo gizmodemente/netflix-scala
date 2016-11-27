df09d1a287f4a4e72bb069eeee67312b4cabd14c
gizmodemente <gizmodemente@googlemail.com>
2016-11-27 18:15:49 +0100

    Add swagger dependencies and couchbase dependencies and properties to connect couchbase database

--- test-service/src/main/scala/org/gizmosoft/scala/application.scala
+++ test-service/src/main/scala/org/gizmosoft/scala/application.scala
@@ -3,12 +3,14 @@ package org.gizmosoft.scala
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
   * Created by gizmo on 21/11/16.
   */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 class application
 
 object Application extends App {
